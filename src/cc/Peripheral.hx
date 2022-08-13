8eef5d386498abe62afd0be9df0526e3d32d9e62
Ed Jones <edjones98@gmail.com>
2022-08-13 18:21:04 +0100

    Pack `peripheral.find `results

--- src/cc/Peripheral.hx
+++ src/cc/Peripheral.hx
@@ -1,6 +1,7 @@
 package cc;
 
 import lua.Table;
+import lua.TableTools;
 import haxe.Constraints;
 import haxe.extern.Rest;
 
@@ -11,6 +12,12 @@ extern class Peripheral {
 	public static function getMethods(name:String):Table<String, Function>;
 	public static function call<T>(name:String, method:String, args:Rest<Dynamic>):T;
 	public static function wrap<T>(name:String):Null<T>;
-	public static function find<T>(type:String, ?filter:(String, T) -> Bool):Table<Int, T>;
+
+	@:native("find")
+	private static function _find<T>(type:String, ?filter:(String, T) -> Bool):Dynamic;
+	public static function find<T>(type:String, ?filter:(String, T) -> Bool):Table<Int, T> {
+		return TableTools.pack(_find(type, filter));
+	}
+
 	public static function getNames():Table<Int, String>;
 }
